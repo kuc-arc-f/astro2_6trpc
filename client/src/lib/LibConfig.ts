1a6a7572df9449d152c3f895c5d65e8fb8ba58fe
中島浩二 <knaka0209@gmail.com>
2023-03-04 12:02:31 +0900

    up, 0304a4

--- client/src/lib/LibConfig.ts
+++ client/src/lib/LibConfig.ts
@@ -6,7 +6,7 @@ const LibConfig = {
   API_URL: "http://localhost:3000",
   //
   SQLITE_FILE_PATH: "/app.sqlite",
-  STORAGE_KEY_DB: 'sqlite_db_astro5' ,
+  STORAGE_KEY_DB: 'sqlite_db_astro6' ,
 
   //session_key
   SESSION_KEY_USER: "user",
